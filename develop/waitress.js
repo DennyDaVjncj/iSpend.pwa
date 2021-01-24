31842143062e94b3974ff51e94a7a4dc97c5197a
Denny da Vjncj <daVjncjCode@gmail.com>
2021-01-24 14:27:36 -0500

    new PORT

--- develop/waitress.js
+++ develop/waitress.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 8347;
+const PORT = 8341;
 
 const app = express();
 
