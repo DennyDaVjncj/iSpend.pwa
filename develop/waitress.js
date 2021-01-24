e852c3ae743ae364fd72ff5b0918eb67382f7d7e
Denny da Vjncj <daVjncjCode@gmail.com>
2021-01-24 14:23:55 -0500

    squashed syntax bug

--- develop/waitress.js
+++ develop/waitress.js
@@ -24,5 +24,5 @@ mongoose.connect("mongodb://localhost/budget", {
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port: http://localhost:${PORT}!`);
+  console.log(`App running on port: http://localhost:${PORT}`);
 });
