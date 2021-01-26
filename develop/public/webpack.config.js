ef7042e993c9bbad65f9254e2b9430b059c01ff7
Denny da Vjncj <daVjncjCode@gmail.com>
2021-01-26 14:11:12 -0500

    tentative configuration

--- develop/public/webpack.config.js
+++ develop/public/webpack.config.js
@@ -4,6 +4,46 @@ const passage=require('path');
 const compass={
     entry:{
         app:'../waitress.js',
-        
-    }
+        homepage:'./index.html',
+        interfaceLogic:'./index.js'
+    },
+    output:{
+        path:__dirname+'/dist',
+        filename:'[name].bundle.js',
+    },
+    mode:'development',
+    module:{
+        rules:[
+            {
+                test:/\.js$/,
+                exclude:/node_modules/,
+                use:{
+                    loader:'babel-loader',
+                    options:{
+                        presets:['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
+    plugins:[
+        new WebpackPwaManifest({
+            fingerprints:false,
+            name:'iSpend.pwa',
+            short_name:'iSpend.pwa',
+            description:'An app for tracking financial transacations with offline capabilities.',
+            background_color:'#fff',
+            theme_color:'#fff',
+            'theme-color':'light-pink',
+            start_url:'/',
+            icons:[
+                {
+                    src:passage.resolve('/icons/icon-192x192.png'),
+                    sizes:[96,128,192,256,384,512],
+                    destination:passage.join('')//resolve ASAP
+                }
+            ]
+        })
+    ]
 }
+module.exports=compass;
