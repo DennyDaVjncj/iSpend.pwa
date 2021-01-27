4044859166e17a4b97583836158d2cd39867f4cc
Denny da Vjncj <daVjncjCode@gmail.com>
2021-01-27 02:00:38 -0500

    removed files from caching

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,8 @@
 const FILES_TO_CACHE = [
     '/',
-    '/index.html',
-    '/favorites.html',
-    '/topic.html',
+    '/index.html',    
     '/assets/css/style.css',
     '/dist/app.bundle.js',
-    '/dist/favorites.bundle.js',
-    '/dist/topic.bundle.js',
     'https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap',
     'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
   ];
