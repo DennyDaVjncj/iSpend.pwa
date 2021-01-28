4d315459f1403a425803bc91c703a40c5f9c5fbb
Denny da Vjncj <daVjncjCode@gmail.com>
2021-01-28 17:19:19 -0500

    connection made

--- waitress.js
+++ waitress.js
@@ -18,7 +18,7 @@ app.use(express.static("public"));
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false,
-  // useUnifiedTopology:true
+  useUnifiedTopology:true
 });
 
 // routes
