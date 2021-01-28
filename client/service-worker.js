13eab5f349185ea4c9f5e78b8d8e94e08a61969f
Denny da Vjncj <daVjncjCode@gmail.com>
2021-01-28 17:31:43 -0500

    updated directory name

---
+++ client/service-worker.js
@@ -0,0 +1,54 @@
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',   
+    '/style.css',
+    '/dist/app.bundle.js',
+    'https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap',
+    'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
+  ];
+
+const PRECACHE='precache-v1';
+const RUNTIME='runtime';
+
+self.addEventListener('install',UX=>{
+    UX.waitUntil(
+        caches
+            .open(PRECACHE)
+            .then(Cache=>Cache.addAll(FILES_TO_CACHE))
+            .then(self.skipWaiting())
+    )
+});
+self.addEventListener('activate',UX=>{
+    const pistashios=[PRECACHE,RUNTIME];
+    UX.waitUntil(
+        caches
+            .keys()
+            .then(cacheNames=>{
+                return cacheNames.filter(cacheName=>!pistashios.includes(cacheName));
+            }).then(cachesToDelete=>{
+                return Promise.all(
+                    cachesToDelete.map(cacheToDelete=>{
+                        return caches.delete(cacheToDelete)
+                    })
+                );
+            }).then(()=>self.clients.claim())
+    );
+});
+self.addEventListener('fetch',UX=>{
+    if(UX.request.url.startsWith(self.location.origin)){
+        UX.respondsWith(
+            caches.match(UX.request).then(cachedResponse=>{
+                if(cachedResponse){
+                    return cachedResponse
+                };
+                return caches.open(RUNTIME).then(cache=>{
+                    return fetch(UX.request).then(response=>{
+                        return cache.put(UX.request,response.clone()).then(()=>{
+                            return response;
+                        })
+                    })
+                })
+            })
+        )
+    }
+});
---
+++ client/webpack.config.js
@@ -0,0 +1,49 @@
+const WebpackPwaManifest=require('webpack-pwa-manifest');
+const passage=require('path');
+
+const compass={
+    entry:{
+        app:'index.js',
+        homepage:'index.html',
+        interfaceLogic:'./index.js'
+    },
+    output:{
+        path:__dirname+'/client/dist',
+        filename:'[name].bundle.js',
+    },
+    mode:'development',
+    module:{
+        rules:[
+            {
+                test:/\.js$/,
+                exclude:/node_modules/,
+                use:{
+                    loader:'babel-loader',
+                    options:{
+                        presets:['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
+    plugins:[
+        new WebpackPwaManifest({
+            fingerprints:false,
+            name:'iSpend.pwa',
+            short_name:'iSpend.pwa',
+            description:'An app for tracking financial transacations with offline capabilities.',
+            background_color:'#fff',
+            theme_color:'#fff',
+            'theme-color':'light-pink',
+            start_url:'/',
+            icons:[
+                {
+                    src:passage.resolve('/icons/icon-192x192.png'),
+                    sizes:[96,128,192,256,384,512],
+                    destination:passage.join('/icons')//tentative rout
+                }
+            ]
+        })
+    ]
+}
+module.exports=compass;
