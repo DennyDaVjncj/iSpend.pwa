43f646e0ad4ff5864b16f0a88c5b84c27377bbd5
DennyDaVjncj <daVjncjCode@gmail.com>
2024-07-21 13:21:48 -0400

    waitress makeover

--- waitress.js
+++ waitress.js
@@ -8,7 +8,6 @@ const PORT = process.env.PORT||8341;
 const app = express();
 
 app.use(logger("dev"));
-
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
